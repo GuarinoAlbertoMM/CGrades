ea82d51f939c69bd5cd0edac9ce703ee211a51e0
Guarino Mendoza <guarinoalbertom@gmail.com>
2024-01-09 22:14:03 -0400

    Final version, modifications to create .exe

--- CGrades/Properties/AssemblyInfo.cs
+++ CGrades/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CGrades")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Gestor de calificaciones.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Guarino Mendoza")]
 [assembly: AssemblyProduct("CGrades")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Guarino Mendoza")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
